3781d1783b5d6fbfd13f91da95678d97fa8539ac
anvcodes <kpeace0720@gmail.com>
2020-01-29 13:01:36 -0500

    client and server

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+        
+      </div>
+    )
+  }
+
+};
+
+export default App;
---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.static("client/build"));
+
+
+app.listen(PORT, function(){
+    console.log(
+        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+        PORT
+      );
+})
